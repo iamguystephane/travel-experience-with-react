acc2967bdd406a23ba75c34e81e5316447352213
iamguystephane <gstephane138@gmail.com>
2024-06-29 00:57:37 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import './App.css'
+import travelArray from './card'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+let travelElements = travelArray.map(item =>{
+  return <Hero 
+    key = {item.id}
+    item = {item}
+  />
+})
+
+function NavBar(){
+  return(
+    <h4> My travel journal </h4>
+  );
+}
+
+
+function Hero(props){
+  return(
+    <div className = "travel-card">
+      <div class = "travel-img">
+        <img src = {props.item.image} />
+      </div>
+      <div class = "travel-details">
+        <div class = "travel-details-header">
+          <h4> {props.item.location} </h4>
+          <a href = {props.item.googleMapsUrl}> view on google maps </a>
+        </div>
+        <h2> {props.item.title}</h2>
+        <b><p> {props.item.startDate} - {props.item.endDate} </p></b>
+        <p> {props.item.description} </p>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default function(){
+  return(
+    <>
+      <nav>
+        <NavBar />
+      </nav>
+      <main>
+        {travelElements}
+      </main>
+    </>
+  )
+}
---
+++ src/card.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://www.google.com/maps/place/Mount+Fuji/@35.3606,138.7274,12z",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing 3776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in japan, for both Japanese and foreign tourists.",
+        image: "/images/mtfuji.jpg"
+    },
+    {
+        id: 2,
+        title: "Sydney Opera House",
+        location: "Australia",
+        googleMapsUrl: "https://www.google.com/maps/place/Sydney+Opera+House/@-33.8567844,151.2152967,15z",
+        startDate: "27 May, 2021",
+        endDate: "8 June, 2021",
+        description: "The Sydney Opera House is a multi-venue performing arts center in Sydney, located on the banks of Sydney harbour. It is often regarded as one of the 20th century's most famous and distinctive building.",
+        image: "/images/sydneyhouse.jpg"
+    },
+    {
+        id: 3,
+        title: "Geirangerfjord",
+        location: "Norway",
+        googleMapsUrl: "https://www.google.com/maps/place/Geirangerfjord/@62.1049112,7.0050036,12z",
+        startDate: "01 Oct, 2021",
+        endDate: "18 Nov, 2021",
+        description: "The Geirangerfjord, located in the Sunnmøre region of Møre og Romsdal county in Norway, is one of the country's most famous fjords and a UNESCO World Heritage site.",
+        image: "/images/geirangerfjord.webp"
+    }
+]
